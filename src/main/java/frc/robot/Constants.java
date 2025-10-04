5f0e1c734dacb768ae705d1e93e992b1306cc899
5933software <jmrobotics@jmchs.org>
2025-10-04 17:59:12 -0600

    begun working on autobuilder stuff for pathplanner

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -32,7 +32,7 @@ public class Constants {
     */ 
     public static final class AlgaeConstants {
 
-        public static final int wheelMotorId = 1;
+        public static final int wheelMotorId = 6;
 
     }
 }
--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -1,11 +1,24 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkMax;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.config.RobotConfig;
+import com.pathplanner.lib.controllers.PPLTVController;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
+import com.studica.frc.AHRS;
+import com.studica.frc.AHRS.NavXComType;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Configs;
@@ -18,6 +31,13 @@ public class DriveSubsystem extends SubsystemBase {
     private final SparkMax right4;
 
     private DifferentialDrive differentialDrive;
+
+    private AHRS gyro = new AHRS(NavXComType.kMXP_SPI);
+
+    public DifferentialDriveOdometry m_odometry;
+
+    DifferentialDriveKinematics kinematics =
+        new DifferentialDriveKinematics(Units.inchesToMeters(21.5));
 
     public DriveSubsystem() {
 
@@ -32,10 +52,50 @@ public class DriveSubsystem extends SubsystemBase {
         right4.configure(Configs.KitbotConfigs.driveRightFollowerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
         differentialDrive = new DifferentialDrive(left1, right2);
+
+        m_odometry = new DifferentialDriveOdometry(
+            new Rotation2d(gyro.getYaw()),
+            left1.getEncoder().getPosition(), right2.getEncoder().getPosition(),
+            new Pose2d());
+
+        RobotConfig config;
+        try {
+            config = RobotConfig.fromGUISettings();
+        } catch (Exception e) {
+            // Handle exception as needed
+            e.printStackTrace();
+        }
+
+        // Configure AutoBuilder last
+        AutoBuilder.configure(
+            this::getPose, // Robot pose supplier
+            this::resetPose, // Method to reset odometry (will be called if your auto has a starting pose)
+            this::getRobotRelativeSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+            (speeds, feedforwards) -> driveRobotRelative(speeds), // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds. Also optionally outputs individual module feedforwards
+            new PPLTVController(0.02), // PPLTVController is the built in path following controller for differential drive trains
+            config, // The robot configuration
+            () -> {
+            // Boolean supplier that controls when the path will be mirrored for the red alliance
+            // This will flip the path being followed to the red side of the field.
+            // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+
+            var alliance = DriverStation.getAlliance();
+            if (alliance.isPresent()) {
+                return alliance.get() == DriverStation.Alliance.Red;
+            }
+            return false;
+            },
+            this // Reference to this subsystem to set requirements
+        );
     }
 
     @Override
-    public void periodic() {}
+    public void periodic() {
+        m_odometry.update(
+            new Rotation2d(gyro.getYaw()),
+            left1.getEncoder().getPosition(), right2.getEncoder().getPosition()
+        );
+    }
 
     @Override
     public void simulationPeriodic() {}
@@ -73,6 +133,27 @@ public class DriveSubsystem extends SubsystemBase {
     public void tankDrive(double leftVal, double rightVal) {
         setLeftMotors(leftVal);
         setRightMotors(rightVal);
+    }
+
+    public Pose2d getPose() {
+        return m_odometry.getPoseMeters();
+    }
+
+    public void resetPose(Pose2d pose) {
+        m_odometry.resetPose(pose);
+    }
+
+    public ChassisSpeeds getRobotRelativeSpeeds() {
+        return ChassisSpeeds.fromRobotRelativeSpeeds(
+            (double) gyro.getRobotCentricVelocityX(),
+            0,
+            gyro.getRate(),
+            new Rotation2d(gyro.getYaw())
+        );
+    }
+
+    public void driveRobotRelative(ChassisSpeeds speeds) {
+        differentialDrive.
     }
 
 }
