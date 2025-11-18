7db5a30f232faec3e8aeb5e6e12591574926453e
Nixon Draper <nd29059@jmchs.org>
2025-11-18 08:22:51 -0700

    created new branch for disco

---
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -0,0 +1,9 @@
+package frc.robot.subsystems;
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.LEDPattern;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class LEDSubsystem extends SubsystemBase {
+    
+}
